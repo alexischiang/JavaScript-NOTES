3215a4b6a83a56c0c0542718b8ab30bd8057abfa
whitelord <1091285927@qq.com>
2018-12-02 13:39:30 +0800

    Create reduce example.js

---
+++ reduce example.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>菜鸟教程(runoob.com)</title>
+</head>
+<body>
+
+<p>点击按钮计算数组元素相加后的总和。</p>
+<button onclick="myFunction()">点我</button>
+
+<p>数组元素总和: <span id="demo"></span></p>
+
+<script>
+    var numbers = [65, 44, 12, 4];
+    function myFunction(){
+    document.getElementById("demo").innerHTML = numbers.reduce((acc,currentValue) =>{
+        return acc + currentValue;
+    },0);
+}
+</script>
+
+</body>
+</html>
